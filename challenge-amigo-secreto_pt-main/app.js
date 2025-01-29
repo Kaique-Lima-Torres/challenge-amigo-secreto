a7502d01ea07c3bd771c7a7239cac01d36618fff
kaique lima torres <kaiquelimat@hotmail.com>
2025-01-29 18:31:50 -0300

    inicio 2025/01/29

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+var listaDeAmigo = [];
+var exibirListaDeAmigo = document.getElementById('listaAmigos');
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo');
+
+    if (nome.value.trim() == "") {
+        alert("Por favor, digite um nome.");
+        return;
+    }else {
+        listaDeAmigo.push(nome.value);
+        //limparcampo();
+        exibirListaDeAmigo();
+    }
+}
+
+function exibirListaDeAmigo(){
+    
+}
+
+
