b53b68226c61c859b7c2313790cd10e46c79bedd
kaique lima torres <kaiquelimat@hotmail.com>
2025-01-30 18:20:53 -0300

    challenge-amigo-secreto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,22 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-var listaDeAmigo = [];
-var exibirListaDeAmigo = document.getElementById('listaAmigos');
+//Aqui você deverá desenvolver a lógica para resolver o problema.
+var listaDeAmigos = [];
+var exibirListaNaTela = document.getElementById('listaAmigos');
+
 
 function adicionarAmigo() {
     let nome = document.getElementById('amigo');
-
-    if (nome.value.trim() == "") {
-        alert("Por favor, digite um nome.");
-        return;
+    
+    if(nome.value.trim() == "") {
+        alert('Por favor, adicione um nome na lista.');
+        return
     }else {
-        listaDeAmigo.push(nome.value);
-        //limparcampo();
-        exibirListaDeAmigo();
+        listaDeAmigos.push(nome.value);
+        limparCampo();
+        exibirAmigosNaTela();
+        
     }
 }
 
-function exibirListaDeAmigo(){
-    
+function sortearAmigo() {
+    if(listaDeAmigos.length == 0) {
+        alert('Não há nenhum nome para ser sorteado!');
+        return
+    } else {
+        let amigoSecreto = document.getElementById('resultado')
+        let gerarAmigoSecreto = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+        amigoSecreto.innerHTML = `O amigo secreto é ${gerarAmigoSecreto}`;
+    }
 }
 
+function exibirAmigosNaTela() {
+        exibirListaNaTela.innerHTML = '';
+        listaDeAmigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        exibirListaNaTela.appendChild(li);
+    });
+
+}
+
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+    
+}
 
